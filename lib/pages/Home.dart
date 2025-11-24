4659086e6d81077bf9815eb3fefaf145744a534b
Mhmd Shami <mhmddshamii@gmail.com>
2025-11-24 19:51:30 +0200

    Home Wlecome

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
+import 'converter.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Welcome"), centerTitle: true),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Welcome to Unit Converter 👋",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                "Convert Length, Weight and Temperature easily.",
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 25),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const ConverterPage()),
+                  );
+                },
+                child: const Text("Start Converting"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

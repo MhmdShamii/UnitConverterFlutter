1a9e421fb761e74f578ca42757bc39bd2bf54936
Mhmd Shami <mhmddshamii@gmail.com>
2025-11-24 19:53:25 +0200

    Home Wlecome

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:unit_converter/widgets/converter_form.dart';
+import './pages/Home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const UnitConverterApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class UnitConverterApp extends StatelessWidget {
+  const UnitConverterApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Unit Converter')),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: ConverterForm(),
-        ),
-      ),
+      title: "Unit Converter",
+      home: const HomePage(),
     );
   }
 }
